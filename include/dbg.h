129c595a76525ae60df148b5e2bfe749f06afd8d
Andy Roulin <andy.roulin@epfl.ch>
2015-01-20 16:41:31 +0100

    ansi pedantic debug macros at the cost of only printing strings, no variadic args nor int/float args

--- include/dbg.h
+++ include/dbg.h
@@ -4,28 +4,33 @@
 #include <stdio.h>
 #include <errno.h> 
 #include <string.h>
-#include "term_color.h"
+
+#define TERM_RED    "\x1b[31;1m"
+#define TERM_GREEN  "\x1b[32;1m"
+#define TERM_YELLOW "\x1b[33;1m"
+#define TERM_WHITE  "\x1b[1m"
+#define TERM_COLOR_X "\x1b[m"
 
 #ifdef NDEBUG
-#define debug(M, ...)
+#define debug(MSG)
 #else 
-#define debug(M, ...) fprintf(stderr, "DEBUG (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define debug(MSG) fprintf(stderr, "DEBUG (%s:%d:%s) %s \n", __FILE__, __LINE__, __func__, MSG)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
  
-#define log_err(M, ...) fprintf(stderr, TERM_RED "[ERROR]" TERM_COLOR_X " (%s:%d:%s, errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
+#define log_err(MSG) fprintf(stderr, TERM_RED "[ERROR]" TERM_COLOR_X " (%s:%d:%s, errno: %s) %s\n", __FILE__, __LINE__, __func__, clean_errno(), MSG)
  
-#define log_warn(M, ...) fprintf(stderr, TERM_YELLOW "[WARN]" TERM_COLOR_X " (%s:%d:%s, errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__) 
+#define log_warn(MSG) fprintf(stderr, TERM_YELLOW "[WARN]" TERM_COLOR_X " (%s:%d:%s, errno: %s) %s\n", __FILE__, __LINE__, __func__, clean_errno(), MSG)
  
-#define log_info(M, ...) fprintf(stderr, TERM_WHITE "[INFO]" TERM_COLOR_X " (%s:%d:%s) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__) 
+#define log_info(MSG) fprintf(stderr, TERM_WHITE "[INFO]" TERM_COLOR_X " (%s:%d:%s) %s\n", __FILE__, __LINE__, __func__, MSG)
 
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
+#define check(A, MSG) if(!(A)) { log_err(MSG); errno=0; goto error; } 
 
-#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__); errno=0; goto error; } 
+#define sentinel(MSG)  { log_err(MSG); errno=0; goto error; } 
 
 #define check_mem(A) check((A), "Out of memory.")
  
-#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; } 
+#define check_debug(A, MSG) if(!(A)) { debug(MSG); errno=0; goto error; } 
 
 #endif
